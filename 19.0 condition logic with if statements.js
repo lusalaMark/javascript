8e6d5ce128aa0ac33279d4670a98b1725be685c2
lusalaMark <lusalamark63@gmail.com>
2020-10-29 10:38:55 +0300

    condition logic with if statements

---
+++ 19.0 condition logic with if statements.js
@@ -0,0 +1,15 @@
+//Example
+function ourTrueOrFalse(isItTrue){
+    if (isItTrue){
+        return "Yes, it's true";
+    }
+    return "No, it's false"
+}
+
+function trueOrFalse(waasThatTrue){
+    if(waasThatTrue){
+        return "Yes";
+    }
+    return "No";
+}
+console.log(trueOrFalse(true));
