2d38a3eeb4572571c4507f4cfbd2b7b20508b586
lusalaMark <lusalamark63@gmail.com>
2020-10-29 07:36:37 +0300

    Arrays

---
+++ 9.0 store multiple values with arrays.js
@@ -0,0 +1,85 @@
+var ourArray = ["Mark", 1996];
+
+var myArray = ["Quincy", 1];
+
+//Nested Arrays
+urArray = [ ["rk", 96], ["Mrk", 196] ,"Mark", 1996];
+
+//Access arrays data with index 
+var marksScored = [60, 70,80,90];
+
+var firstStudentScoreontheScoreshit = marksScored[0];
+
+console.log(firstStudentScoreontheScoreshit);
+
+// modifying arrays data with indexes
+
+var myData = [2,3,4,5,6,7];
+
+myData[1] = 90;
+console.log(myData); 
+
+//Accessing multiple-Dimensional arrays with indexes
+
+// set up
+var myArray = [[1,2,3], [4,5,6],[7,8,9],[[10,22,23],[12,45,67,89,34],[3,6,4,8,9]]]
+
+// only change the code below 
+var myData = myArray[2][1]
+console.log(myData);
+
+var ourAray = ["stimpson","j","cat"];
+ourAray.push(["happy","joy"]);
+
+var myArray = [["john",23],["cat",2]];
+
+myArray.push(["dog", 3])
+
+console.log(ourArray);
+console.log(myArray);
+
+//manipulate arrays with pop()
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+console.log(ourArray);
+
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.shift();
+console.log(ourArray);
+
+var ourAray = ["stimpson","j","cat"];
+ourAray.shift();
+ourArray.unshift("happy");
+
+var myArray = [["john",23],["cat",2]];
+
+myArray.shift();
+console.log(ourArray);
+console.log(myArray);
+
+//Manipulate arrays with push()
+var myArray = ["junior", "analia"];
+myArray.push(["sana"]);
+console.log(myArray);
+
+//manmipulate arrays with pop()
+var myArray = [["john",23],["cat",2]];
+
+var removedFromMyArray = myArray.pop();
+console.log(myArray);
+
+//manipulate arrays with shift() 
+
+var myArr = [["mark", 4], ["emma", 5]];
+var removedFromMyArr = myArr.shift();
+console.log(myArr);
+
+//manipulate arrays with unshift ()
+var myAr = [["mark", 4], ["emma", 5]];
+
+myAr.unshift(["aul", 40]);
+
+console.log(myAr);
+
+//shopping list
+var myList = [["cearals", 3],["milk",4],["bananas",7],["juice",10],["eggs",6]];
