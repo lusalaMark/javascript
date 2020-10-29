fb1f5ccf1630d468a71e66ea120df6bdc5c079ed
lusalaMark <lusalamark63@gmail.com>
2020-10-29 12:38:19 +0300

    golf game

---
+++ 21.0 comparison with strict equality operator.js
@@ -0,0 +1,32 @@
+//set up
+function testEqual(val){
+    if (val !== 12){
+        return "Yes";
+    }
+    return " No";
+}
+
+//change  the value to test 
+console.log(testEqual(125)); 
+
+//set up
+function teEqual(val){
+    if (val === 12){
+        return "Equal";
+    }
+    return " Not Equal";
+}
+
+//change  the value to test 
+console.log(teEqual(12)); 
+
+//set up
+function tEqual(val){
+    if (val >= 2){
+        return "Eqal";
+    }
+    return " Not Equal";
+}
+
+//change  the value to test 
+console.log(tEqual(12)); 
